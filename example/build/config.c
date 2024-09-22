8fd8ba12022d4cf433e2b0593f33dfc1677fb73e
MrKleeblatt <mrkleeblatt@gmail.com>
2024-09-22 11:38:52 +0200

    examples on how to use it

---
+++ example/build/config.c
@@ -0,0 +1,16 @@
+#include "devm_api.h"
+#include "x-watcher/array.h"
+
+struct version_s version = {0, 1};
+
+void load_config(config* conf){
+
+	conf->compiler = "ccache clang";
+	arr_add(conf->compile_flags, "-Wall");
+
+	source main_source = devm_source("src/main.c");
+	arr_add(main_source.extra_flags, "-Wno-gnu-statement-expression");
+	arr_add(conf->sources, main_source);
+
+	arr_add(conf->sources, devm_source("src/test.c"));
+}
